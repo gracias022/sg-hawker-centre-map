2892b86cf5e96bc1e94163fbe184e6c84b56e56c
Gracia Chew <159509493+gracias022@users.noreply.github.com>
2026-03-20 12:12:59 +0800

    style(layout): center map view and refine page styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,16 @@
-import { MapContainer, TileLayer, Marker, Tooltip } from "react-leaflet";
 import "leaflet/dist/leaflet.css"; // Required for map rendering
 import "./App.css";
+import MapComponent from "./components/MapComponent";
 
 function App() {
-  const singaporeCenter = [1.3521, 103.8198];
-
   return (
-    <div
-      className="map-wrapper"
-      style={{ display: "flex", flexDirection: "column" }}
-    >
-      <MapContainer
-        center={singaporeCenter}
-        zoom={11}
-        scrollWheelZoom={true}
-        style={{
-          height: "400px",
-          width: "95%",
-          marginBottom: "2rem",
-          alignSelf: "center",
-        }}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-
-        {/* Dummy marker */}
-        <Marker position={singaporeCenter}>
-          <Tooltip direction="top" offset={[-15, -20]} opacity={1}>
-            <div style={{ padding: "5px" }}>
-              <strong>Singapore Central</strong> <br />
-              <span>Address: </span>
-              <br />
-              <small>Postal Code: 123456</small>
-            </div>
-          </Tooltip>
-        </Marker>
-      </MapContainer>
+    <div className="App">
+      <header className="App-header">
+        <h3>Singapore Hawker Centres Map</h3>
+      </header>
+      <main className="App-main">
+        <MapComponent />
+      </main>
     </div>
   );
 }
